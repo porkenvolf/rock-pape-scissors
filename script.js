9133afd6ec8d89346a303d62c3903eaac55fe3f7
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-05-07 16:40:32 -0300

    Write playRound function
    It takes two options and declares a winner. Parameter 1 represents
    the human player and parameter 2 represents the computer.

--- script.js
+++ script.js
@@ -12,4 +12,31 @@ function getComputerChoice() {
   }
   return choice;
 }
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  console.log(playerChoice);
+  console.log(computerChoice);
+  let result;
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "rock") result = "It's a tie";
+      if (computerChoice === "paper") result = "You Lose! Paper beats Rock";
+      if (computerChoice === "scissors")
+        result = "You Win! Rock beats Scissors";
+      break;
+    case "paper":
+      if (computerChoice === "rock") result = "You Win! Paper beats Rock";
+      if (computerChoice === "paper") result = "It's a tie";
+      if (computerChoice === "scissors")
+        result = "You Lose! Scissors beat Paper";
+      break;
+    case "scissors":
+      if (computerChoice === "rock") result = "You Lose! Rock beats Scissors";
+      if (computerChoice === "paper") result = "You Win! Scissors beat Paper";
+      if (computerChoice === "scissors") result = "It's a tie";
+      break;
+  }
+  return result;
+}
+console.log(playRound(getComputerChoice(), getComputerChoice()));
